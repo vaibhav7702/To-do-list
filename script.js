c2db9483dafcd0078cb5ace299b7faf56ed6e482
vaibhav7702 <73778348+vaibhav7702@users.noreply.github.com>
2021-11-27 15:00:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
+function inputLength(){
+	return input.value.length;
+} 
+
+function listLength(){
+	return item.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+
+
+	//START STRIKETHROUGH
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");
+	}
+
+	li.addEventListener("click",crossOut);
+	//END STRIKETHROUGH
